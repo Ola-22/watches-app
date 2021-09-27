01c9e30d83cc2452a0f1abcfa98cb337d9763b5a
Ola-22 <olanazly@gmail.com>
2021-09-27 22:36:59 +0300

    add slider component

---
+++ src/Components/SliderTop/SliderContent/index.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+export default function SliderContent({ title, paragraph }) {
+  return (
+    <div className="content-main">
+      <h1>{title}</h1>
+      <p>{paragraph}</p>
+      <a href="#">Discover</a>
+      <div>
+        <button>
+          <img src="/images/add-cart.png" alt="" />
+          <h6>Add to cart</h6>
+        </button>
+        <button>
+          <img src="/images/logo-cicil.png" alt="" />
+        </button>
+      </div>
+    </div>
+  );
+}
