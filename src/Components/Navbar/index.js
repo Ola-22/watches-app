dfc8e028eac04edc89ac7a1ae611a0c332a18cbe
Ola-22 <olanazly@gmail.com>
2021-09-27 22:37:20 +0300

    add file

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,25 @@
+import "./style.css";
+
+export default function Navbar() {
+  return (
+    <div className="nav-main">
+      <>
+        <img src="/images/logo.png" alt="" />
+      </>
+      <nav className="menu-main">
+        <a href="#">Watches</a>
+        <a href="#">Eyewear</a>
+        <a href="#">Accessories</a>
+        <a href="#">News</a>
+      </nav>
+      <div className="nav-box">
+        <img src="/images/search.png" alt="" />
+        <div className="user-box">
+          <img src="/images/user.png" alt="" />
+          <h6>Log In</h6>
+        </div>
+        <img src="/images/cart.png" alt="" />
+      </div>
+    </div>
+  );
+}
