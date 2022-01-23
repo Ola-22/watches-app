0292e758353e84637161d6b8c2aaa4dc8e8da803
Ola-22 <olanazly@gmail.com>
2022-01-23 21:25:32 +0200

    Add data of Testimonials

---
+++ src/Components/Testimonials/Slider/SliderContent.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    desc: "Loving my new KAILI watch from @matoa_id, the first ever Indonesian watch local brand that uses wood as their main material. Like any other Matoa products, KAILI is inspired by Indonesian heritage.",
+    name: "Gita Savitri",
+    job: "Content Creator/Influencer",
+    img: "/images/testimonial.png",
+  },
+  {
+    desc: "2 Loving my new KAILI watch from @matoa_id, the first ever Indonesian watch local brand that uses wood as their main material. Like any other Matoa products, KAILI is inspired by Indonesian heritage.",
+    name: "Lama Savitri",
+    job: "Computer Engineering",
+    img: "/images/testimonial.png",
+  },
+  {
+    desc: "3 Loving my new KAILI watch from @matoa_id, the first ever Indonesian watch local brand that uses wood as their main material. Like any other Matoa products, KAILI is inspired by Indonesian heritage.",
+    name: "Amanda Savitri",
+    job: "E-Marketing",
+    img: "/images/testimonial.png",
+  },
+];
