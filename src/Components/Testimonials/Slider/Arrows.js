3e32ca8c6f7c0c029a777b45a3a2b4ab55fcfb34
Ola-22 <olanazly@gmail.com>
2022-01-24 19:09:48 +0200

    Add left and right arrows

--- src/Components/Testimonials/Slider/Arrows.js
+++ src/Components/Testimonials/Slider/Arrows.js
@@ -2,10 +2,10 @@ export default function Arrows({ prevSlide, nextSlide }) {
   return (
     <div className="arrows">
       <span className="prev" onClick={prevSlide}>
-        &#10094;
+        <img src="/images/left.png" alt="" />
       </span>
       <span className="next" onClick={nextSlide}>
-        &#10095;
+        <img src="/images/right.png" alt="" />
       </span>
     </div>
   );
