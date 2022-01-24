33a37e3c8fc5ef82054cb5517dc1039cdd4621f4
Ola-22 <olanazly@gmail.com>
2022-01-24 19:09:04 +0200

    Add Image to slider component

--- src/Components/Testimonials/Slider/Slider.js
+++ src/Components/Testimonials/Slider/Slider.js
@@ -1,17 +1,25 @@
 export default function Slider({ activeIndex, SliderContent }) {
   return (
-    <div>
+    <>
       {SliderContent.map((slide, index) => (
         <div
           key={index}
           className={index === activeIndex ? "slides active" : "inactive"}
         >
-          <img src={slide.img} alt="" />
-          <p>{slide.desc}</p>
-          <h4>{slide.name}</h4>
-          <h6>{slide.job}</h6>
+          <div>
+            <img src="/images/Rectangle.png" alt="" />
+
+            <img src={slide.img} alt="" />
+          </div>
+
+          <div>
+            <h1>Testimonials</h1>
+            <p>{slide.desc}</p>
+            <h4>{slide.name}</h4>
+            <h6>{slide.job}</h6>
+          </div>
         </div>
       ))}
-    </div>
+    </>
   );
 }
