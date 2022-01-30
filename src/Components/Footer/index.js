374964a25f8cf116a49bd56cd87d673d4c4ba971
Ola-22 <olanazly@gmail.com>
2022-01-30 21:09:54 +0200

    create footer Main

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,10 @@
+import FooterTop from "./FooterTop";
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <FooterTop />
+    </div>
+  );
+}
