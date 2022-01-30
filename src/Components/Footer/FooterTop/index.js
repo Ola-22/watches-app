3b665c78f13fd221ac83433a2da0f7798b4c0011
Ola-22 <olanazly@gmail.com>
2022-01-30 21:12:52 +0200

    Add images Payment in footer

---
+++ src/Components/Footer/FooterTop/index.js
@@ -0,0 +1,20 @@
+export default function FooterTop() {
+  return (
+    <div className="footer-top">
+      <div>
+        <img src="/images/Payment1.png" alt="" />
+        <img src="/images/Payment2.png" alt="" />
+        <img src="/images/Payment3.png" alt="" />
+        <img src="/images/Payment4.png" alt="" />
+        <img src="/images/Payment5.png" alt="" />
+      </div>
+      <div>
+        <img src="/images/Payment6.png" alt="" />
+        <img src="/images/Payment7.png" alt="" />
+        <img src="/images/Payment8.png" alt="" />
+        <img src="/images/Payment9.png" alt="" />
+        <img src="/images/Payment10.png" alt="" />
+      </div>
+    </div>
+  );
+}
