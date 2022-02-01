e022f9fe450c618459aa80a5434096ddc6735ed8
Ola-22 <olanazly@gmail.com>
2022-02-01 20:40:57 +0200

    Add footerBottom component in footer main

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,10 +1,12 @@
 import FooterTop from "./FooterTop";
 import "./style.css";
+import FooterBottom from "./FooterBottom";
 
 export default function Footer() {
   return (
     <div className="footer">
       <FooterTop />
+      <FooterBottom />
     </div>
   );
 }
