d93013bf9fcc61e4bd7d0f3b8230eb3d831dd5d1
Ola-22 <olanazly@gmail.com>
2022-02-01 20:44:50 +0200

    Add information in footer

---
+++ src/Components/Footer/FooterBottom/index.js
@@ -0,0 +1,58 @@
+export default function FooterBottom() {
+  return (
+    <div className="footer-bottom">
+      <div>
+        <img src="/images/footer.png" alt="" />
+        <h3>Address</h3>
+        <h4>
+          Store & Office Jl. Setrasari Kulon III, No. 10-12, Sukarasa, Sukasari,
+          Bandung, Jawa Barat, Indonesia 40152
+        </h4>
+        <h3>Office Hour</h3>
+        <h4>Monday - Sunday 10.00 - 18.00</h4>
+      </div>
+      <div>
+        <h2>Get in touch</h2>
+        <div>
+          <h3>Phone</h3>
+          <h4>022-20277564</h4>
+        </div>
+        <div>
+          <h3>Service Center</h3>
+          <h4>0811-233-8899</h4>
+        </div>
+        <div>
+          <h3>Customer Service</h3>
+          <h4>0811-235-9988</h4>
+        </div>
+        <div>
+          <img src="/images/Facebook.png" alt="" />
+          <img src="/images/Instagram.png" alt="" />
+          <img src="/images/Twitter.png" alt="" />
+          <img src="/images/Youtube.png" alt="" />
+        </div>
+      </div>
+      <div>
+        <h2>Useful Link</h2>
+        <h4>Warranty & Complaints</h4>
+        <h4>Order & Shipping</h4>
+        <h4>Tracking Order</h4>
+        <h4>About Us</h4>
+        <h4>Repair</h4>
+        <h4>Terms</h4>
+        <h4>FAQ</h4>
+      </div>
+
+      <div>
+        <h2>Campaign</h2>
+        <h4> Mengenal Arti Cukup</h4>
+        <h4>Tell Your Difference</h4>
+        <h4>Waykambas</h4>
+        <h4>Rebrand</h4>
+        <h4>Gallery</h4>
+        <h4>Singo</h4>
+        <h4>Rakai</h4>
+      </div>
+    </div>
+  );
+}
